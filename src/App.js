23ede882ee25275296b5effbbf973f2be364a77f
Matheus <mat.araujoribeiro@gmail.com>
2022-12-05 11:54:19 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Pokemon Finder</h1>
+    </div>
+  );
 }
 
 export default App;
