6149f48bad894af7f9d4712124330723198294a2
Matheus <mat.araujoribeiro@gmail.com>
2022-12-06 11:37:23 -0300

    update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Pokemon Finder</h1>
+    <div
+      style={{
+        margin: "auto",
+        width: 800,
+        paddingTop: "1rem",
+        border: "2px solid black",
+      }}
+    >
+      <h1 className="title">Poke Finder1</h1>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Bulbasaur</td>
+            <td>Grass, Poison</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
